fd7a6493031907049e9197e5eaa75680ec4ed67d
송현정 <songhj9810@gmail.com>
2026-08-07 23:37:06 +0900

    ✏️ 오타 수정

--- src/app/providers/naver-script.tsx
+++ src/app/providers/naver-script.tsx
@@ -6,7 +6,7 @@ export function NaverScript() {
   return (
     <Script
       type="text/javascript"
-      src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&callback=CALLBACK_FUNCTION`}
+      src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID!}&callback=CALLBACK_FUNCTION`}
       strategy="afterInteractive"
     />
   )
